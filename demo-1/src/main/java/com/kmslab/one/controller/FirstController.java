cf78ee9d19af73da80793784d798e474e16ade17
dosa777 <dosa777@gmail.com>
2026-01-14 14:13:33 +0900

    설정

--- demo-1/src/main/java/com/kmslab/one/controller/FirstController.java
+++ demo-1/src/main/java/com/kmslab/one/controller/FirstController.java
@@ -1,16 +1,10 @@
 package com.kmslab.one.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class FirstController {
 	@GetMapping("/hi")
 	public String niceToMeetYou(Model model) {
@@ -22,14 +16,5 @@ public class FirstController {
 	public String seeYouNext(Model model) {
 		model.addAttribute("nickname", "이런 젠");
 		return "goodbye";
-	}
-	
-	
-	@Autowired
-	private MongoTemplate mongoTemplate;
-	@GetMapping("/api/{collectionName}")
-	public List<Map> findAll(@PathVariable("collectionName") String collectionName){
-		return mongoTemplate.findAll(Map.class, collectionName);
-	}
-	
+	}	
 }
